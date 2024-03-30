4b6751e6d0372e93aa2940d9f307c2cd381a637c
“watchgou” <zjon234@gmail.com>
2024-03-30 22:42:47 +0800

    log

--- raft-server/src/main.rs
+++ raft-server/src/main.rs
@@ -1,9 +1,19 @@
-use log::info;
-use raft_code::protos;
-use raft_common::raft_state::State;
 fn main() {
-    let hello = protos::hello::HotWordRequest::default();
-    let value = State::default();
-    info!("State: {:?}", value);
-    info!("HotWordRequest: {}", hello);
+    println!("hello world");
+}
+
+#[cfg(test)]
+mod test {
+    use env_logger;
+    use log::info;
+    use raft_code::protos;
+    use raft_common::raft_state::State;
+    #[test]
+    fn test() {
+        env_logger::init();
+        let hello = protos::hello::HotWordRequest::default();
+        let value = State::default();
+        info!("State: {:#?}", value);
+        info!("HotWordRequest: {:?}", hello);
+    }
 }
