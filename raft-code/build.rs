9b9125d5e262c419c8815b8f705009d24d908310
“watchgou” <zjon234@gmail.com>
2024-04-01 21:14:12 +0800

    build protobuf

--- raft-code/build.rs
+++ raft-code/build.rs
@@ -5,9 +5,9 @@ fn main() {
         // Use `protoc-bin-vendored` bundled protoc command, optional.
         .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
         // All inputs and imports from the inputs must reside in `includes` directories.
-        .includes(&["src/protos"])
+        .includes(&["./protos"])
         // Inputs must reside in some of include paths.
-        .input("src/protos/hello.proto")
+        .input("./protos/hello.proto")
         // Specify output directory relative to Cargo output directory.
         .out_dir("src/protos")
         //.cargo_out_dir("protos")
