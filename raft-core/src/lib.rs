f101c445c6380a8db6cbe431abebfbc40a85de87
“watchgou” <zjon234@gmail.com>
2024-04-04 12:53:40 +0800

    x

--- raft-core/src/lib.rs
+++ raft-core/src/lib.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use raft_common::*;
+
+#[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
+pub enum State {
+    // 追随者
+    #[default]
+    Followers,
+
+    // 领导者
+    Leader,
+
+    // 候选者
+    Candidate,
+}
+
+pub fn start() {
+    // 初始化 过滤器
+    filter::FilterChain::init();
 }
